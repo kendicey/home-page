bbd8152b9b86d648ea5c5e70ed38f864ede3dd53
kendice <kendiceyeungnm@gmail.com>
2023-10-13 17:28:35 -0700

    tech stack to icons & use S3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,9 +15,24 @@ const Contact = () => {
             </div>
             <div className='card' style={{textAlign: "center", border: "0"}}>
                 <div className='card-body' style={{margin: "auto"}}>
-                    <a href='https://www.linkedin.com/in/kendicenmyeung/' className='contact_links' target='_blank'><button type="button" class="btn btn-outline-secondary linkedinBtn">LinkedIn</button></a><br/>
-                    <a href='https://github.com/kendicey' className='contact_links' target='_blank'><button type="button" class="btn btn-outline-secondary githubBtn">GitHub</button></a><br/>
-                    <button onClick={handleCopy} type="button" class="btn btn-outline-secondary" id="emailBtn">Email</button>
+                    <a href='https://www.linkedin.com/in/kendicenmyeung/' className='contact_links' target='_blank' rel="noreferrer">
+                        <button type="button" class="btn btn-outline-secondary linkedinBtn">
+                            <img src='https://s3.us-west-2.amazonaws.com/kendicey.io/contact_linkedin.png' alt='linkedin' className='contact_icons' />
+                            LinkedIn
+                        </button>
+                    </a>
+                    <br/>
+                    <a href='https://github.com/kendicey' className='contact_links' target='_blank' rel="noreferrer">
+                        <button type="button" class="btn btn-outline-secondary githubBtn">
+                            <img src='https://s3.us-west-2.amazonaws.com/kendicey.io/contact_github.png' alt='github' className='contact_icons' />
+                            GitHub
+                        </button>
+                    </a>
+                    <br/>
+                    <button onClick={handleCopy} type="button" class="btn btn-outline-secondary" id="emailBtn">
+                        <img src='https://s3.us-west-2.amazonaws.com/kendicey.io/contact_email.png' alt='email' className='contact_icons' />
+                        Email
+                    </button>
                 </div>
             </div>
         </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,20 +1,19 @@
-import React, {useEffect, useState} from 'react';
-import askaiden from '../assets/askaiden.png';
+import React, { useState } from 'react';
 
 const ProjectCard = (props) => (
     <div className='card project' style={{margin: "3rem 0.5rem", padding: "1.5rem"}}>
         <div className='card-body'></div>
             <div className='row'>
                 <div className='col-md-12 col-lg-5'>
-                    <img src={props.name === "AskAiden" ? askaiden : props.image} alt='project image' className='img-fluid projectImg' style={{borderRadius: "20px", marginBottom: "20px"}}/>
+                    <img src={props.image} alt='project image' className='img-fluid projectImg' style={{borderRadius: "20px", marginBottom: "20px"}}/>
                 </div>
                 <div className='col-md-12 col-lg-7'>
                     <h5 className='card-title'>{props.name}</h5>
                     <p className='card-text'>{props.description}</p>
-                    <ul>
+                    <ul className='project_desc'>
                         {props.works.map((work) => <li>{work}</li>)}
                     </ul>
-                    <p className='card-text' style={{fontWeight: "bold"}}>Tech stack: {props.technologies}</p>
+                    <p className='card-text' style={{fontWeight: "bold"}}>{props.technologies.map(t => <img src={`https://s3.us-west-2.amazonaws.com/kendicey.io/${t}.png`} alt={t} className={t === "express" ? "express_icon" : "home_icons"} />)}</p>
                     <a href={props.url} target='_blank' className='project_links'>
                         <button type="button" className="btn btn-outline-secondary" style={{marginBottom: "1rem"}}>
                         view project
@@ -35,7 +34,6 @@ const Project = (props) => {
                 <h2>My Projects</h2>
                 <p>Welcome to my project gallery! Explore my previous projects to see examples of my work <span style={{whiteSpace: "nowrap"}}>(* •̀ᴗ•́ *)و ̑̑</span></p>
             </div>
-                {console.log(projects[0].props.projects)}
                 {projects[0].props.projects.map((project) => 
                     <ProjectCard 
                         key={project.id}
